92f54c32a3118892dd59ccbe80981d0acd8ff4a1
MihaiNuica97 <mihainuica@gmail.com>
2020-02-28 02:06:45 +0000

    implemented file chooser. function actually returns a File object now

--- src/main/java/segGroupCW/App.java
+++ src/main/java/segGroupCW/App.java
@@ -14,27 +14,33 @@ import java.io.IOException;
 public class App extends Application {
 
 
-    private static Scene scene;
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"));
-        stage.setScene(scene);
-        stage.show();
-    }
+	private static Scene scene;
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
+	public static Scene getScene(){
+		return scene;
+	}
+	@Override
+	public void start(Stage stage) throws IOException {
+		scene = new Scene(loadFXML("fileUpload"));
 
-    }
+		stage.setScene(scene);
+		stage.show();
+	}
 
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+	 static void setRoot(String fxml) throws IOException {
+		scene.setRoot(loadFXML(fxml));
 
-    public static void main(String[] args) {
-        launch();
-    }
+	}
+
+
+	private static Parent loadFXML(String fxml) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+		return fxmlLoader.load();
+	}
+
+	public static void main(String[] args) {
+		launch();
+	}
 
 }
--- src/main/java/segGroupCW/FileLoadController.java
+++ src/main/java/segGroupCW/FileLoadController.java
@@ -1,39 +1,55 @@
 package segGroupCW;
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 
-import java.io.IOException;
+import java.io.File;
 
 public class FileLoadController {
 
-    private FileChooser fileChooser;
-    @FXML
-    private Button browseClicksBtn;
-    @FXML
-    private Button browseImprBtn;
-    @FXML
-    private Button browseServerBtn;
+	private FileChooser fileChooser;
 
-    public void initialize(){
-        browseClicksBtn.setOnAction( e -> browseFiles("clicks"));
-        browseImprBtn.setOnAction( e -> browseFiles("impressions"));
-        browseServerBtn.setOnAction( e -> browseFiles("server"));
-    }
+	private Scene scene;
+	@FXML
+	private Button browseClicksBtn;
 
-    @FXML
-    private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
-    }
+	@FXML
+	private TextField clicksPathLabel;
+
+	@FXML
+	private Button browseImprBtn;
+	@FXML
+	private TextField imprPathLabel;
+
+	@FXML
+	private TextField serverPathLabel;
+	@FXML
+	private Button browseServerBtn;
+
+	public void initialize(){
+		fileChooser = new FileChooser();;
+        browseClicksBtn.setOnAction( e -> browseFiles(clicksPathLabel,"Clicks Log"));
+		browseImprBtn.setOnAction( e -> browseFiles(imprPathLabel,"Impressions"));
+		browseServerBtn.setOnAction( e -> browseFiles(serverPathLabel,"Server Log"));
+	}
 
 
-    public void browseFiles(String fileType){
-        System.out.println("Choosing file with type: " + fileType);
-//        fileChooser.showOpenDialog(.publicStage.getWindow());
 
+	public File browseFiles(TextField field, String fileType){
+		System.out.println("Choosing file with type: " + fileType);
+		FileChooser.ExtensionFilter fileExtensions = new FileChooser.ExtensionFilter(fileType,"*.csv");
+		fileChooser.getExtensionFilters().add(fileExtensions);
+		fileChooser.setSelectedExtensionFilter(fileExtensions);
 
-    }
+		File file = fileChooser.showOpenDialog(App.getScene().getWindow());
+		field.setText(file.getAbsolutePath());
+
+		return file;
+	}
+
 
 
 }
