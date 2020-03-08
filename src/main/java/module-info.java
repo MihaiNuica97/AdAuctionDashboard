a1f7d9ddb852d6e90b660015b0b43c5628ac0b97
MihaiNuica97 <mihainuica@gmail.com>
2020-03-08 20:43:46 +0000

    added jfoenix properly

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module segGroupCW {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+	requires com.jfoenix;
 
-    opens segGroupCW to javafx.fxml;
+	opens segGroupCW to javafx.fxml;
     exports segGroupCW;
 }
