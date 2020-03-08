2d26b2ba7fe25d54e94ae237d7a4b66878eb5b61
Charlotte <charlotte.j.crisp@gmail.com>
2020-03-08 17:51:32 +0000

    Added some custom statements with javadoc comments

--- src/main/java/segGroupCW/SQLCreator.java
+++ src/main/java/segGroupCW/SQLCreator.java
@@ -27,7 +27,7 @@ public class SQLCreator {
     /**
      * SQL statement to calculate the total impressions.
      *
-     * @return    string of the SQL statement
+     * @return    String of the SQL statement
      */
     private String numOfImps() {
         return "SELECT COUNT(*) FROM impressions;";
@@ -36,28 +36,28 @@ public class SQLCreator {
     /**
      * SQL statement to calculate the total clicks.
      *
-     * @return      string of the SQL statement
+     * @return      String of the SQL statement
      */
     private String numOfClicks() { return "SELECT COUNT(*) FROM clicks;";}
 
     /**
      * SQL statement to calculate the total clicks from unique users.
      *
-     * @return      string of the SQL statement
+     * @return      String of the SQL statement
      */
     private String numOfUniques() { return "SELECT COUNT(DISTINCT (ID)) FROM CLICKS;";}
 
     /**
      * SQL statement to calculate the total bounces. Default bounce definition is only 1 page viewed.
      *
-     * @return      string of the SQL statement
+     * @return      String of the SQL statement
      */
     private String numOfBounces() { return "SELECT COUNT(*) FROM SERVER WHERE PAGESVIEWED = 1;";}
 
     /**
      * SQL statement to calculate the total conversions.
      *
-     * @return      string of the SQL statement
+     * @return      String of the SQL statement
      */
     private String numOfConvs() { return "SELECT COUNT (*) FROM SERVER WHERE CONVERSION = TRUE;";}
 
@@ -134,17 +134,69 @@ public class SQLCreator {
     }
 
 
-    private String numOfImps(String start, String end) {
-        return "SELECT COUNT(*) FROM impressions WHERE DATE => " + start + " AND DATE <= " + end + ";";
-    }
-
-    private String numOfClicks(String start, String end) {
-        return "SELECT COUNT(*) FROM clicks WHERE DATE => " + start + " AND DATE <= " + end + ";";
-    }
-
-    /* Consider splitting "between date" or "unique ID" or "cost greater than"
+    /* Started customisation statements
+     * splitting out "between date" or "unique ID" or "cost greater than"
      * into their own functions so that they can be reused by each different metric
      * ie. if you want clicks with between a date the statement is clicks + date(start, end)
      * but you can also do impressions + date(start, end)
      */
+
+    /**
+     * SQL statement for a generic where statement between 2 dates. Both dates should be in YYYY-MM-DD [HH:MM:SS] format.
+     *
+     * @param start The earlier date
+     * @param end   The later date
+     * @return      String of the SQL statement
+     */
+    private String betweenDate(String start, String end) {
+        return "DATE => TIMESTAMP '" + start + "' AND DATE" + " <= '" + end + "'";
+    }
+
+    /**
+     * SQL statement for the total impressions between 2 dates. Both dates should be in YYYY-MM-DD [HH:MM:SS] format.
+     *
+     * @param start The earlier date
+     * @param end   The later date
+     * @return      String of the SQL statement
+     */
+    private String numOfImps(String start, String end) {
+        return "SELECT COUNT(*) FROM impressions WHERE " + betweenDate(start, end) + ";";
+    }
+
+    /**
+     * SQL statement for the total clicks between 2 dates. Both dates should be in YYYY-MM-DD [HH:MM:SS] format.
+     *
+     * @param start The earlier date
+     * @param end   The later date
+     * @return      String of the SQL statement
+     */
+    private String numOfClicks(String start, String end) {
+        return "SELECT COUNT(*) FROM clicks WHERE " + betweenDate(start, end) + ";";
+    }
+
+    /**SQL statement for total bounces based on time spent on the site. The user can define the
+     * unit used and how many of said unit. Unit can be second, minute, hour, day, month, or year.
+     * <p>
+     * Ex. unit as seconds and time as 10 means stays under 10 seconds will be counted as bounces
+     *
+     * @param unit  What unit the time is in
+     * @param time  How long the visit needs to be to not be a bounce
+     * @return      String of the SQL statement
+     */
+    private String bounceByTime(String unit, String time) {
+        return "SELECT COUNT(*) FROM SERVER WHERE DATEDIFF(" + unit + ", ENTRYDATE, EXITDATE) < " + time;
+    }
+
+    /**
+     * SQL statement for total bounces based on pages visited. The user can define
+     * the minimum no. of pages visited for visit to not be a bounce.
+     * <p>
+     * Ex. pages as 3 means visits of 1 or 2 pages are bounces and 3 pages are not
+     *
+     * @param pages Number of pages to not count as a bounce
+     * @return      String of the SQL statement
+     */
+    private String bounceByPages(String pages) {
+        return "SELECT COUNT(*) FROM SERVER WHERE PAGESVIEWED < " + pages;
+    }
 }
