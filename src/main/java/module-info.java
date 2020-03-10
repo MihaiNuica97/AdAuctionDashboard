a9cf7e596264387c14fe248b267c6909544aa5ba
Crisp-y <charlotte.j.crisp@gmail.com>
2020-03-10 13:23:58 +0000

    added junit dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module segGroupCW {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires org.junit.jupiter.api;
 
     opens segGroupCW to javafx.fxml;
     exports segGroupCW;
