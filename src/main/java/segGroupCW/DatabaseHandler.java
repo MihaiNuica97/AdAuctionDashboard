7349468cedc68cdfe4c70ca6c4d56a97fa12763c
Mihai Nuica <mihainuica@gmail.com>
2020-03-11 12:42:41 +0000

    added import to db handler

--- src/main/java/segGroupCW/DatabaseHandler.java
+++ src/main/java/segGroupCW/DatabaseHandler.java
@@ -1,10 +1,6 @@
 package segGroupCW;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-
+import java.sql.*;
 
 
 public class DatabaseHandler {
